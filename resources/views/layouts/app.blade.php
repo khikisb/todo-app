b7d9f737571ffc76a5ddacaed0f6f7cb2bcdd115
khikisb <89850965+khikisb@users.noreply.github.com>
2025-06-07 00:53:35 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,6 +125,6 @@
     </div>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
